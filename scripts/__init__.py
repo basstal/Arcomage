cf9d06747157742a2d0af6c76ecc1b32da407e63
basstal <330475004@qq.com>
2022-11-02 18:15:27 +0800

    remove scripts folder

--- scripts/__init__.py
+++
@@ -1,8 +0,0 @@
-import sys
-from importlib import import_module
-import imp
-from os.path import dirname
-
-sys.path.append(dirname(__file__))
-# for item in sys.path:
-    # print("Script running path>>>[%s]"%(item))
